5b6edb0e58373553477ef23d673c3ad1bda0c963
AvaNdreyWeb <avandrey.web@gmail.com>
2023-05-08 19:08:34 +0300

    Support of Environment Variables

--- hwc/database.py
+++ hwc/database.py
@@ -1,12 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-DB_TYPE = 'postgresql'
-DB_USER = 'postgres'
-DB_PASS = 'postgres'
-DB_HOST = 'localhost:5432'
-DB_NAME = 'hwc_db'
+DB_TYPE = os.environ.get('DB_TYPE')
+DB_USER = os.environ.get('DB_USER')
+DB_PASS = os.environ.get('DB_PASS')
+DB_HOST = os.environ.get('DB_HOST')
+DB_NAME = os.environ.get('DB_NAME')
 
 SQLALCHEMY_DATABASE_URL = f"{DB_TYPE}://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}"
 
